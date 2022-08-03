c8708e9bce265769076223f10820f95884178ea6
notiamanees <aneesh5sidhu@gmail.com>
2022-08-02 17:07:11 -0700

    my first checkin
    my first checkin

---
+++ index.js
@@ -0,0 +1,9 @@
+let firstName = "Bro"
+let age = 21
+let student = true
+age = age + 1
+console.log("Hello", firstName)
+console.log("You are", age, "years old")
+console.log("Enrolled:", student)
+
+document.getElementById("p1").innerHTML = "This is the new p1";
